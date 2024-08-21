c5f4d550e8ae3ef4be41406ab216f1d7d0944a3c
gunali14 <151998537+gunali14@users.noreply.github.com>
2024-08-21 11:54:39 +0000

    added a style to a softwaredeveloper page & add a daily report (21/08/2024)
    Signed-off-by: gunali14 <151998537+gunali14@users.noreply.github.com>

--- src/components/service/SoftwareDeveloper.tsx
+++ src/components/service/SoftwareDeveloper.tsx
@@ -1,40 +1,70 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './SoftwareDeveloper.css';
 
 const SoftwareDeveloper: React.FC = () => {
+
+    useEffect(() => {
+        const sections = document.querySelectorAll('.fade-in-section');
+
+        const observer = new IntersectionObserver((entries) => {
+            entries.forEach(entry => {
+                if (entry.isIntersecting) {
+                    entry.target.classList.add('is-visible');
+                }
+            });
+        }, { threshold: 0.1 });
+
+        sections.forEach(section => {
+            observer.observe(section);
+        });
+
+        return () => {
+            sections.forEach(section => observer.unobserve(section));
+        };
+    }, []);
+
     return (
         <>
             {/* Marketing Section */}
-            <div className='body d-flex'>
-      
-            <div className="flex flex-row p-5">
-              <div className="content p-10">
-               <h1 className='text-blue-600 text-2xl'>Software</h1>
-               <br></br>
-               Lorem ipsum dolor sit amet consectetur adipisicing elit. <br></br>
-               Obcaecati quidem nostrum ea! Veniam, accusamus? Tempore <br></br>libero recusandae id temporibus voluptas dolorem. Laudantium fugiat, cumque cum aliquid temporibus error ullam vel!
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit.<br></br> Tempora distinctio nihil provident numquam quis magnam repellendus quasi nostrum eveniet minima obcaecati architecto minus libero harum maiores ipsa, dolor necessitatibus vel.
-              </div>
-              <div className="img p-10">
-              <img src="public/Software-Logos/img-1.png" alt="Marketing" />
-              </div>
-            </div>
-            <div className="flex flex-row-reverse p-5">
-              <div className="content p-10">
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempora distinctio nihil provident numquam quis magnam repellendus quasi nostrum eveniet minima obcaecati architecto minus libero harum maiores ipsa, dolor necessitatibus vel.
-              </div>
-              <div className="img p-10">
-              <img src="public/Software-Logos/img-1.png" alt="Marketing" />
-              </div>
-            </div>
-            <div className="flex flex-row p-5">
-              <div className="content p-10">
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempora distinctio nihil provident numquam quis magnam repellendus quasi nostrum eveniet minima obcaecati architecto minus libero harum maiores ipsa, dolor necessitatibus vel.
-              </div>
-              <div className="img p-10">
-              <img src="public/Software-Logos/img-1.png" alt="Marketing" />
-              </div>
-            </div>
+            <div className='body'>
+
+                <div className="flex p-5 fade-in-section">
+                    <div className="content p-10">
+                        <h1 className='text-blue-600 text-2xl'>Software</h1>
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore recusandae vel porro et hic impedit ullam soluta ipsum quos fugiat sit eum quidem ipsam laborum dicta dolorum, laudantium id odio.
+                            Obcaecati quidem nostrum ea! Veniam, accusamus? Tempore libero recusandae id temporibus voluptas dolorem. Laudantium fugiat, cumque cum aliquid temporibus error ullam vel!
+                        </p>
+                    </div>
+                    <div className="img p-10">
+                        <img src="public/Software-Logos/img-1.png" alt="Marketing" />
+                    </div>
+                </div>
+                <div className='reverse-content'>
+                 <div className="flex p-5 fade-in-section" >
+                    <div className="img p-10">
+                        <img src="public/Software-Logos/img-2.jpg" alt="Marketing" />
+                    </div>
+                    <div className="content p-10">
+                    <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore recusandae vel porro et hic impedit ullam soluta ipsum quos fugiat sit eum quidem ipsam laborum dicta dolorum, laudantium id odio.
+                            Obcaecati quidem nostrum ea! Veniam, accusamus? Tempore libero recusandae id temporibus voluptas dolorem. Laudantium fugiat, cumque cum aliquid temporibus error ullam vel!
+                        </p>
+                    </div>
+                </div>
+                </div>
+                <div className="flex p-5 fade-in-section">
+                    <div className="content p-10">
+                    <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore recusandae vel porro et hic impedit ullam soluta ipsum quos fugiat sit eum quidem ipsam laborum dicta dolorum, laudantium id odio.
+                            Obcaecati quidem nostrum ea! Veniam, accusamus? Tempore libero recusandae id temporibus voluptas dolorem. Laudantium fugiat, cumque cum aliquid temporibus error ullam vel!
+                        </p>
+                    </div>
+                    <div className="img p-10">
+                        <img src="public/Software-Logos/img3.jpg" alt="Marketing" />
+                    </div>
+                </div>
+            
             </div>
         </>
     );
